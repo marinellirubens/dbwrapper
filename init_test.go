f18bbf5b2fc51ee2393a6ddf418b4f1e46061452
marinellirubens <marinelli.rubens@gmail.com>
2023-05-01 16:16:17 -0300

    included first test just as an example

---
+++ init_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_GetInfoFile(t *testing.T) {
+	_, err := GetInfoFile("./config.ini")
+	if err != nil {
+		t.Errorf("GetInfoFile - ERROR %v", err)
+	} else {
+		t.Logf("GetInfoFile - SUCCESS")
+	}
+}
