10d72e8696c4951a7050a4be3d52a1d537a4d2a2
marinellirubens <marinelli.rubens@gmail.com>
2024-12-27 00:14:47 -0300

    Changed scripts

--- scripts/curl_example.sh
+++ scripts/curl_example.sh
@@ -1,1 +1,1 @@
-curl -s -w '%{time_total}' http://localhost:8080/pg\?query\="select+*+from+public.Employee" -o /dev/null
+curl -s -w '%{time_total}' http://localhost:8080/pg\?query\="select+*+from+public.Employee"
