8a255a79025e09a3eb5dea62b841ee85a9bea5ef
marinellirubens <marinelli.rubens@gmail.com>
2024-12-30 17:58:14 -0300

    Changed imports for test

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -2,13 +2,11 @@ package config
 
 import (
 	"testing"
-
-	cf "github.com/marinellirubens/dbwrapper/internal/config"
 )
 
 func Test_GetInfoFile(t *testing.T) {
 	//fmt.Println(os.Getwd())
-	_, err := cf.GetInfoFile("./examples/config.example.ini")
+	_, err := GetInfoFile("./examples/config.example.ini")
 	if err != nil {
 		t.Errorf("GetInfoFile - ERROR %v", err)
 	} else {
