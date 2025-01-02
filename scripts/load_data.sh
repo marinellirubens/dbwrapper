d3d4871024b963854af4f709591802e42e95cfc3
marinellirubens <marinelli.rubens@gmail.com>
2025-01-02 09:34:37 -0300

    Corrected load scripts

--- scripts/load_data.sh
+++ scripts/load_data.sh
@@ -1,2 +1,2 @@
-sudo docker cp ./sql/dummy_data.sql postgresql:/tmp
-sudo docker exec -ti postgresql psql -U myusername -f /tmp/dummy_data.sql
+sudo docker cp ./sql/dummy_data.sql local_postgres:/tmp
+sudo docker exec -ti local_postgres psql -U myusername -f /tmp/dummy_data.sql
--- scripts/postgres_container_create.sh
+++ scripts/postgres_container_create.sh
@@ -1,7 +1,7 @@
 docker run -p 5432:5432 \
-    -v /tmp/database:/var/lib/postgresql/data \
+    -v pgsql_data:/var/lib/postgresql/data \
     -e POSTGRES_PASSWORD=mypassword \
     -e POSTGRES_USER=myusername \
-    -e POSTGRES_DB=postgres \
+    -e POSTGRES_DB=myusername \
     --name local_postgres \
     -d postgres
